c3537a29752c073cb5b90fa2681cb51578cb6ee6
shahdqutp89 <ext.shahd.qutp@appraid-tech.com>
2025-09-18 09:59:33 +0000

    Format code with Black

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-
 import argparse
 from arxml_manager import ARXMLManagerFactory
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Process ARXML file with attribute addition')
-    parser.add_argument('input_file', help='Input ARXML file path')
-    parser.add_argument('output_file', help='Output ARXML file path')
-    
+    parser = argparse.ArgumentParser(
+        description="Process ARXML file with attribute addition"
+    )
+    parser.add_argument("input_file", help="Input ARXML file path")
+    parser.add_argument("output_file", help="Output ARXML file path")
+
     arguments = parser.parse_args()
-    
+
     manager = ARXMLManagerFactory.create_standard_manager()
     manager.load_arxml_file(arguments.input_file)
     manager.add_attribute_by_tag("ECUC-MODULE-CONFIGURATION-VALUES", "version", "1.0")
     manager.save_arxml_file(arguments.output_file)
+
 
 if __name__ == "__main__":
     main()
